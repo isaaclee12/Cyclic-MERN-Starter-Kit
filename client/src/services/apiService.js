1ee2e16ba6f23f477a213d94c64d184e19470aec
isaaclee12 <59675296+isaaclee12@users.noreply.github.com>
2023-02-04 20:43:23 -0500

    Attempting to insert cors/access headers

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const URL = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
   headers: {"Access-Control-Allow-Origin": "*", "mode": "cors"},
 });
 
